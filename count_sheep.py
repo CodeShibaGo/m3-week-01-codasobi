b0e8aa269bc7d0c3e36f7355f06bbc50a66263d3
codasobi <carol.tang79@gmail.com>
2024-01-09 20:50:55 -0700

    feat: add count sheep

--- count_sheep.py
+++ count_sheep.py
@@ -1,2 +1,6 @@
-def count_sheep(sheep):
-    pass
+def count_sheep(sheeps):
+    sleep_sheeps = 0
+    for sheep in sheeps:
+        if sheep == True:
+            sleep_sheeps += 1
+    return sleep_sheeps

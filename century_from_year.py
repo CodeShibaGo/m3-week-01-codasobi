44d4d732ab83b847a56c5e9ff5fc98a3b9cce67a
codasobi <carol.tang79@gmail.com>
2024-01-09 19:19:22 -0700

    feat: add century_from_year

--- century_from_year.py
+++ century_from_year.py
@@ -1,2 +1,13 @@
+# 1705 --> 18
+# 1900 --> 19
+# 1601 --> 17
+# 2000 --> 20
+
 def century(year):
-    pass
+    str_year = str(year)
+    if str_year[2] + str_year [3] == '00':
+        return int(str_year[0] + str_year[1])
+    else:
+        return int(str_year[0] + str_year[1]) + 1
+
+century(2321)

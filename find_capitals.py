6b0e4770dcff33d0e3478e2c5fccd122aef9e38d
codasobi <carol.tang79@gmail.com>
2024-01-09 22:35:09 -0700

    fix: find capitals logic

--- find_capitals.py
+++ find_capitals.py
@@ -1,7 +1,11 @@
 def find_capitals(word):
     word_split = word.split()
     capitals = ''
-    for word_split in word_split:
-        capitals += word_split[0]
+
+    for word in word_split:
+        for char in word:
+            if char.isupper():
+                capitals += char
+
     return capitals
 

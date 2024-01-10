2f7696c840f19727605d24ec8455850db88ce18a
codasobi <carol.tang79@gmail.com>
2024-01-09 18:54:09 -0700

    feat: add categorize new member

--- categorize_new_member.py
+++ categorize_new_member.py
@@ -1,2 +1,5 @@
 def categorize_new_member(data):
-    pass
+    categorize = lambda x: 'Senior' if x[0] >= 55 and x[1] > 7 else 'Open'
+    member_category = list(map(categorize, data))
+
+    return member_category
